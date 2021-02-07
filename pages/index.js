5854f2d23688f569523e18e8200778a902c48188
sam@apostel.be <sam@apostel.be>
2021-02-07 20:20:22 +0100

    add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	const colors = 6;
+	const designs = 5;
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	const renderSweaters = () => {
+		const sweaters = [];
+		for (let color = 1; color <= colors; color++){
+			for (let design = 1; design <= designs; design++) {
+				sweaters.push({color, design});
+			}
+		}
+		return (
+			<div className={styles.gallery}>
+				{sweaters.sort(() => Math.random() - 0.5).map(renderSweater)}
+			</div>
+		);
+	};
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+	const renderSweater = ({color, design}, index) => {
+		return (
+			<figure
+				key={`color-${color}_design_${design}`}
+				className={styles.shopItem}
+			>
+				<img
+					src={`/sweaters/color-${color}_design-${design}.png`} alt={`sweater color ${color} with design ${design}`}
+					className={styles.shopItemImage}
+				/>
+			</figure>
+		);
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+	}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+			<main className={styles.main}>
+				{renderSweaters()}
+			</main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			{/*<footer className={styles.footer}>*/}
+			{/*	/!*<a*!/*/}
+			{/*	/!*	href="https://bunkerstore.be"*!/*/}
+			{/*	/!*	target="_blank"*!/*/}
+			{/*	/!*	rel="noopener noreferrer"*!/*/}
+			{/*	/!*>*!/*/}
+			{/*	<span>*/}
+			{/*		Powered by{' '}*/}
+			{/*		<img src="/bunkerstore.svg" alt="bunkerstore Logo" className={styles.logo} />*/}
+			{/*	</span>*/}
+
+			{/*	/!*</a>*!/*/}
+			{/*</footer>*/}
+		</div>
+	)
 }
